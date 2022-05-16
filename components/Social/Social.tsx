7534cfb5d4f741b7af01dec8708ba1c5699814df
EvgenijZ <leetruk12@gmail.com>
2022-05-16 21:59:12 +0300

    Update Social component

--- components/Social/Social.tsx
+++ components/Social/Social.tsx
@@ -1,25 +1,40 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
 	faYoutube,
-	faFacebook,
-	faVimeo,
+	faFacebookF,
+	faVimeoV,
 } from '@fortawesome/free-brands-svg-icons';
+import styles from './Social.module.scss';
 
-const Social = () => {
+const Social: React.FC = () => {
 	return (
-		<div className='social-container'>
+		<div className={styles.social}>
 			<a
 				href='https://www.facebook.com/learnbuildteach/'
-				className='facebook social'>
-				<FontAwesomeIcon icon={faFacebook} size='2x' />
+				className={`${styles.link} ${'facebook social'}`}>
+				<FontAwesomeIcon
+					className={`${styles.icon} ${styles.facebook}`}
+					icon={faFacebookF}
+					size='2x'
+				/>
 			</a>
 			<a
 				href='https://www.youtube.com/c/jamesqquick'
-				className='youtube social'>
-				<FontAwesomeIcon icon={faYoutube} size='2x' />
+				className={`${styles.link} ${'youtube social'}`}>
+				<FontAwesomeIcon
+					className={`${styles.icon} ${styles.youtube}`}
+					icon={faYoutube}
+					size='2x'
+				/>
 			</a>
-			<a href='https://www.vimeo.com' className='vimeo social'>
-				<FontAwesomeIcon icon={faVimeo} size='2x' />
+			<a
+				href='https://www.vimeo.com'
+				className={`${styles.link} ${'vimeo social'}`}>
+				<FontAwesomeIcon
+					className={`${styles.icon} ${styles.vimeo}`}
+					icon={faVimeoV}
+					size='2x'
+				/>
 			</a>
 		</div>
 	);
