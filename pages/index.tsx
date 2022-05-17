54fafeab38158998160431dbdd653de69d5b9cc3
EvgenijZ <leetruk12@gmail.com>
2022-05-18 01:40:35 +0300

    Added Carousel component to the index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,10 @@
 import type { NextPage } from 'next';
-import Head from 'next/head';
-import Image from 'next/image';
+import Carousel from '../components/Carousel/Carousel';
 
 const Home: NextPage = () => {
 	return (
 		<div>
-			123
-			<Image src='/vercel.svg' alt='Vercel Logo' width={72} height={16} />
+			<Carousel />
 		</div>
 	);
 };
