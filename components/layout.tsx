6bb12216d54e2bb0f73203cb921025920bc4fc9a
EvgenijZ <leetruk12@gmail.com>
2022-05-18 14:46:40 +0300

    Added ThemeContext

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,11 +1,16 @@
 import Footer from './Footer/Footer';
 import Header from './Header/Header';
+import { useContext } from 'react';
+import { ThemeContext } from '../contexts/theme-context';
 
 export default function Layout({ children }) {
+	const themeVariant = useContext(ThemeContext);
+	const prepareStyles = () => (themeVariant === 'light' ? null : 'content');
+
 	return (
 		<>
 			<Header />
-			<main>{children}</main>
+			<main className={prepareStyles()}>{children}</main>
 			<Footer />
 		</>
 	);
