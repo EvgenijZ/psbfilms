87e96570445afa5878ab46dbac3cd071feb96cad
EvgenijZ <leetruk12@gmail.com>
2022-05-18 14:46:22 +0300

    Added Header styles

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,8 +1,9 @@
 import Navbar from '../Navbar/Navbar';
+import styles from './Header.module.scss';
 
 const Header: React.FC = () => {
 	return (
-		<header>
+		<header className={styles.header}>
 			<Navbar />
 		</header>
 	);
