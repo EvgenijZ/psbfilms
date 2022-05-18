89d8f348e048fbc0bb06cd7d8a76ad723af7e4b2
EvgenijZ <leetruk12@gmail.com>
2022-05-18 23:34:49 +0300

    Initial Director page

---
+++ pages/directors.tsx
@@ -0,0 +1,13 @@
+import Directors from '../components/Directors/Directors';
+import Title from '../components/Title/Title';
+
+const DirectorsPage = () => {
+	return (
+		<div>
+			<Title title='Directors' subtitle='Our' />
+			<Directors />
+		</div>
+	);
+};
+
+export default DirectorsPage;
