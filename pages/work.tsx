61983ade836c7f42b53e98fd790effaba969cfd6
EvgenijZ <leetruk12@gmail.com>
2022-05-21 15:32:40 +0300

    Initial Work page

---
+++ pages/work.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from 'next';
+import Title from '../components/Title/Title';
+
+const Work: NextPage = () => {
+	return (
+		<div>
+			<Title title='Portfolio' subtitle='Our' />
+		</div>
+	);
+};
+
+export default Work;
