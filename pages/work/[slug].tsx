03c71f022445cec5811035c6a1cf38d6f50b3702
EvgenijZ <leetruk12@gmail.com>
2022-05-21 19:09:26 +0300

    Move work page

---
+++ pages/work/[slug].tsx
@@ -0,0 +1,13 @@
+import Categories from '../../components/Categories/Categories';
+import Title from '../../components/Title/Title';
+
+const WorkCategory = () => {
+	return (
+		<>
+			<Title title='Portfolio' subtitle='Our' />
+			<Categories />
+		</>
+	);
+};
+
+export default WorkCategory;
