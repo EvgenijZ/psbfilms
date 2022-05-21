98a37e5ef696434b392f3c7db3741d9ea29857c2
EvgenijZ <leetruk12@gmail.com>
2022-05-21 15:34:20 +0300

    Updated Directors page

--- pages/directors.tsx
+++ pages/directors.tsx
@@ -1,11 +1,9 @@
 import Directors from '../components/Directors/Directors';
-import Title from '../components/Title/Title';
 
 const DirectorsPage = () => {
 	return (
 		<div>
-			<Title title='Directors' subtitle='Our' />
-			<Directors />
+			<Directors title='Directors' subtitle='Our' />
 		</div>
 	);
 };
